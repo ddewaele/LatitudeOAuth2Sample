b1e97baaec437c0c0e8001ecbbb531ba42fe5be6
ddewaele <ddewaele@gmail.com>
2011-08-05 19:04:12 +0200

    changed credential store

--- src/main/java/com/ecs/sample/oauth2/latitude/cmdline/oauth/FileBasedCredentialStore.java
+++ src/main/java/com/ecs/sample/oauth2/latitude/cmdline/oauth/FileBasedCredentialStore.java
@@ -27,53 +27,39 @@ public class FileBasedCredentialStore implements CredentialStore {
 
 	private AccessTokenResponse readTokenFromFile(String fileLocation) {
 		AccessTokenResponse accessTokenResponse = new AccessTokenResponse();
-	    try {
-//		    FileInputStream fin = new FileInputStream(fileLocation);
-//		    ObjectInputStream ois = new ObjectInputStream(fin);
-//		    accessTokenResponse = (AccessTokenResponse) ois.readObject();
-//		    ois.close();
-		
-	    	FileInputStream fin = new FileInputStream(fileLocation);
-		DataInputStream in = new DataInputStream(fin);
-		  BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		  accessTokenResponse.accessToken = br.readLine();
-		  accessTokenResponse.expiresIn = Long.parseLong(br.readLine());
-		  accessTokenResponse.refreshToken = br.readLine();
-		  accessTokenResponse.scope = br.readLine();
-		  in.close();
-	    }
-	    catch (Exception ex){
-	      ex.printStackTrace();
-	      return null;
-	    }
+		try {
+			FileInputStream fin = new FileInputStream(fileLocation);
+			DataInputStream in = new DataInputStream(fin);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			accessTokenResponse.accessToken = br.readLine();
+			accessTokenResponse.expiresIn = Long.parseLong(br.readLine());
+			accessTokenResponse.refreshToken = br.readLine();
+			accessTokenResponse.scope = br.readLine();
+			in.close();
+		} catch (Exception ex) {
+			return null;
+		}
 		return accessTokenResponse;
-	  }
+	}
 
-	private void writeFile(String fileLocation,
-			AccessTokenResponse accessTokenResponse) {
+	private void writeFile(String fileLocation,AccessTokenResponse accessTokenResponse) {
 		try {
 			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileLocation));
-	            
-	            //Start writing to the output stream
-	            bufferedWriter.write(accessTokenResponse.accessToken);
-	            bufferedWriter.newLine();
-	            bufferedWriter.write(String.valueOf(accessTokenResponse.expiresIn));
-	            bufferedWriter.newLine();
-	            bufferedWriter.write(accessTokenResponse.refreshToken);
-	            bufferedWriter.newLine();
-	            if (accessTokenResponse.scope!=null) {
-	            	bufferedWriter.write(accessTokenResponse.scope);
-	            	bufferedWriter.newLine();
-	            }
 
-//			FileOutputStream fout = new FileOutputStream(fileLocation);
-//			
-//			ObjectOutputStream oos = new ObjectOutputStream(fout);
-//			oos.writeObject(accessTokenResponse);
-//			oos.close();
-	            bufferedWriter.close();
+			bufferedWriter.write(accessTokenResponse.accessToken);
+			bufferedWriter.newLine();
+			bufferedWriter.write(String.valueOf(accessTokenResponse.expiresIn));
+			bufferedWriter.newLine();
+			bufferedWriter.write(accessTokenResponse.refreshToken);
+			bufferedWriter.newLine();
+			
+			if (accessTokenResponse.scope != null) {
+				bufferedWriter.write(accessTokenResponse.scope);
+				bufferedWriter.newLine();
+			}
+
+			bufferedWriter.close();
 		} catch (Exception ex) {
-			ex.printStackTrace();
 		}
 	}
 
